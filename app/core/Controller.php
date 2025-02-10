ad19b58996da776e87440ca6f4cce702cdf64618
LatrachDev <m.latrach.youcode@gmail.com>
2025-02-10 09:45:00 +0100

    MVC-7 create controller class

---
+++ app/core/Controller.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Core;
+    class Controller 
+    {
+        public function view($view, $data = []) 
+        {
+            extract($data);
+            require_once __DIR__ . '/../views/' . $view . '.php';
+        }
+    }
